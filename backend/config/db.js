fb09c4318f78a4a45e9386fcfcfec8f387d2e4ce
asharmajeed <asharmajeed40@gmail.com>
2024-09-20 18:35:00 +0500

    update db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://aviationuser:aviationuser123@cluster0.kroov.mongodb.net/aviation?retryWrites=true&w=majority&appName=Cluster0");
     console.log("Successfully connected to mongoDb :)");
   } catch (error) {
     console.error(`ERROR: ${error.message}`);
