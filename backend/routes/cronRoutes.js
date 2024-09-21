4937b43660bd5a98d5015d9194c9f680f93d8be0
asharmajeed <asharmajeed40@gmail.com>
2024-09-21 21:25:22 +0500

    change cron

--- backend/routes/cronRoutes.js
+++ backend/routes/cronRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-// import Repair from "../models/repair.js";
+import Repair from "../models/repair.js";
+import moment from "moment";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
@@ -8,7 +9,7 @@ dotenv.config(); // Load environment variables
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  console.log('running');
+  console.log("running");
   const transporter = nodemailer.createTransport({
     service: "gmail", // or 'gmail', depending on your service
     auth: {
@@ -17,22 +18,40 @@ router.post("/", async (req, res) => {
     },
   });
 
-  try {
-    // Add logic to check expired repairs here
-    const isExpired = true; // Example condition for expired repairs
+  const sendExpiryMailToAdmin = (repair) => {
+    const mailOptions = {
+      from: process.env.ADMIN_EMAIL,
+      to: process.env.ADMIN_EMAIL, // Always send to admin
+      subject: `Repair Expired for ${repair.componentType}`,
+      text: `The repair for component "${repair.componentType}" by "${
+        repair.username
+      }" has expired on ${moment(repair.expireDate).format(
+        "ddd MMM DD YYYY"
+      )}. Please take the necessary action.`,
+    };
 
-    if (isExpired) {
-      await transporter.sendMail({
-        from: process.env.ADMIN_EMAIL,
-        to: process.env.ADMIN_EMAIL, // Admin email
-        subject: "Repair Expired",
-        text: "A repair has expired!",
-      });
-      console.log("Email sent");
-      res.status(200).json({ success: true });
-    } else {
-      res.status(200).json({ message: "No expired repairs found" });
-    }
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log("Error sending email:", error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+  };
+
+  try {
+    console.log("Checking for expired repairs...");
+
+    const currentDate = moment().format("YYYY-MM-DD");
+
+    const expiredRepairs = await Repair.find({ expireDate: currentDate });
+    console.log("Expired Repairs:", expiredRepairs);
+
+    // Send an email for each expired repair
+    expiredRepairs.forEach((repair) => {
+      sendExpiryMailToAdmin(repair);
+    });
   } catch (error) {
     console.error("Error sending email:", error);
     res.status(500).json({ error: "Failed to send email" });
